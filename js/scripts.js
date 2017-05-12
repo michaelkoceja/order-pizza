97728442c8f44ad7d025cd28a60d45c9612c1b6d
michaelkoceja <michael.koceja@gmail.com>
2017-05-12 10:53:48 -0700

    creat customer name input, create new customer, new order constructors and prototypes in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,32 @@
 //Business Logic
-//order constructor
-
-
+//new customer constructor
+function Customer(first, last) {
+  this.firstName = first;
+  this.lastName = last;
+  this.order = [];
+}
+//new order constructor
+function Order(sauceType, cheese, meatType, toppings) {
+  this.sauceType = sauceType;
+  this.cheese = cheese;
+  this.meatType = meatType;
+  this.toppings = toppings;
+}
+//new customer prototype
+Customer.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+};
 //new order prototype
-
-
+Order.prototype.newOrder = function() {
+  return this.sauceType + "," this.cheese + "," this.meatType + "," this.toppings;
+}
 
 //User Interface Logic
+$(function() {
+  $("pizzaOrder").submit(function(event) {
 
+    event.preventDefault();
+  });
+});
 
 //gather user inputs
