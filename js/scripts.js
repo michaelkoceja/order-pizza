3b6908530823f905ff3fb76e146e24bec6883f65
michaelkoceja <michael.koceja@gmail.com>
2017-05-12 16:56:36 -0700

    add pizza size option in html, add pizza size to java script, still not working, not recorgnizing 'this'

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,8 @@ function Customer(first, last) {
   this.order = [];
 }
 //new order constructor
-function Order(sauceType, cheese, meatType, toppings) {
+function Order(size, sauceType, cheese, meatType, toppings) {
+  this.size = size;
   this.sauceType = sauceType;
   this.cheese = cheese;
   this.meatType = meatType;
@@ -18,27 +19,32 @@ Customer.prototype.fullName = function() {
 };
 //new order prototype
 Order.prototype.newOrder = function() {
-  return this.sauceType + "," this.cheese + "," this.meatType + "," this.toppings;
+  return this.size + "," this.sauceType + "," this.cheese + "," this.meatType + "," this.toppings;
 }
 
 //User Interface Logic
 $(function() {
-  $("pizzaOrder").submit(function(event) {
+  $("#pizzaOrder").submit(function(event) {
     event.preventDefault();
 
     var inputtedFirstName = $("input#firstName").val();
     var inputtedLastName = $("input#lastName").val();
 
     var newCustomer = new Customer(inputtedFirstName, inputtedLastName);
+    var completeOrder = new Order(inputtedsize, inputtedsauceType, inputtedcheese, inputtedmeatType, inputtedtoppings)
 
     $(".newOrder").each(function() {
+      var inputtedsize = $(this).find("input.size").val();
       var inputtedsauceType = $(this).find("input.sauceType").val();
       var inputtedcheese = $(this).find("input.cheese").val();
       var inputtedmeatType = $(this).find("input.meatType").val();
       var inputtedtoppings = $(this).find("input.toppings").val();
-      var newOrder = new Order(inputtedsauceType, inputtedcheese, inputtedmeatType, inputtedtoppings);
+      var newOrder = new Order(inputtedsize, inputtedsauceType, inputtedcheese, inputtedmeatType, inputtedtoppings);
       newOrder.order.push(newOrder);
       });
+
+      $("ul#completeOrder").append("<li><span class='contact'>" + completeOrder.newOrder() + "</span></li>");
+
 
     });
   });
