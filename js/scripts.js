127de3374adbaa082d11df7cf97a91b9db80ee85
michaelkoceja <michael.koceja@gmail.com>
2017-05-16 22:54:08 -0700

    update html classes and id's

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,79 @@
 //Business Logic
 //new customer constructor
 function Customer(first, last) {
-  this.firstName = first;
+  // this.firstName = first;
   this.lastName = last;
-  this.order = [];
 }
+
 //new order constructor
-function Order(size, sauceType, cheese, meatType, toppings) {
+function PizzaOrder(size, sauceType, cheese, meatType, toppings) {
   this.size = size;
   this.sauceType = sauceType;
   this.cheese = cheese;
   this.meatType = meatType;
   this.toppings = toppings;
 }
+
 //new customer prototype
 Customer.prototype.fullName = function() {
-  return this.firstName + " " + this.lastName;
+  return this.firstName + " " + this.lastName + ",";
 };
-//new order prototype
-Order.prototype.newOrder = function() {
-  return this.size + "," this.sauceType + "," this.cheese + "," this.meatType + "," this.toppings;
-}
+
+//add to total based on selected size
+PizzaOrder.prototype.newOrder = function(array) {
+  var total = 4;
+  if (this.size === "large") {
+    total += 9;
+  } else if (this.size === "medium") {
+    total += 7;
+  } else {
+    total += 5;
+  }
+ //add number of selected toppings to total
+  for(var index = 0; i < this.sauceType.length; index++) {
+    total += 1;
+  }
+  for(var index = 0; i < this.cheese.length; index++) {
+    total += 1;
+  }
+  for(var index = 0; i < this.meatType.length; index++) {
+    total += 1;
+  }
+  for(var index = 0; i < this.toppings.length; index++) {
+    total += 1;
+ }
+};
+//show pizza order information
+PizzaOrder.prototype.showOrder = function () {
+  return "You've orded a " + this.size + "with " + this.sauceType + "sauce, " + this.cheese + ", " + this.meatType.join(", ") + "and " + this.toppings.join(", ") + ".";
+};
 
 //User Interface Logic
 $(function() {
   $("#pizzaOrder").submit(function(event) {
     event.preventDefault();
 
-    var inputtedFirstName = $("input#firstName").val();
-    var inputtedLastName = $("input#lastName").val();
+    // var inputtedFirstName = $("input#firstName").val();
+    // var inputtedLastName = $("input#lastName").val();
+    //
+    // var newCustomer = new Customer(inputtedFirstName, inputtedLastName);
 
-    var newCustomer = new Customer(inputtedFirstName, inputtedLastName);
-    var completeOrder = new Order(inputtedsize, inputtedsauceType, inputtedcheese, inputtedmeatType, inputtedtoppings)
-
-    $(".newOrder").each(function() {
-      var inputtedsize = $(this).find("input.size").val();
-      var inputtedsauceType = $(this).find("input.sauceType").val();
-      var inputtedcheese = $(this).find("input.cheese").val();
-      var inputtedmeatType = $(this).find("input.meatType").val();
-      var inputtedtoppings = $(this).find("input.toppings").val();
-      var newOrder = new Order(inputtedsize, inputtedsauceType, inputtedcheese, inputtedmeatType, inputtedtoppings);
-      newOrder.order.push(newOrder);
-      });
-
-      $("ul#completeOrder").append("<li><span class='contact'>" + completeOrder.newOrder() + "</span></li>");
-
-
+    var selectedSize = $("select#pizzaSize").val();
+    var selectedSauceType = $("select#sauceType").val();
+    var selectedCheese = $("select#cheese").val();
+    var selectedMeatType = $("select#meatType").val();
+    var selectedToppings = $("select#toppings").val();
+    var selectedIngredients = [];
+    $("input:checkbox[name=selected-ingredient]:checked").each(function() {
+      selectedIngredients.push($(this).val());
+      console.log();
     });
+
+    var newOrder = new PizzaOrder(selectedSize, selectedSauceType, selectedCheese, selectedMeatType, selectedToppings);
+
+
+    $("#completeOrder").show();
+    $("#yourOrder").html(newOrder.showOrder())
   });
 });
 
